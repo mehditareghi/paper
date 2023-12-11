d4cdbf1dc65a4c61238f77e8be03e435efc1a476
Mehdi Tareghi <mehditareghi@gmail.com>
2023-12-11 10:57:21 +0330

    continue working on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,51 @@ export default function Home() {
           </div>
         </div>
         <div className='mt-4 md:mt-0'>
-          <Image src='/static/banner.webp' alt='banner' width={500} height={500}/>
+          <Image src='/static/banner.webp' alt='banner' width={500} height={500} />
+        </div>
+      </div>
+      <div className='p-4 py-16 lg:px-48 flex flex-col bg-gray-200 justify-center items-center gap-4'>
+        <h2 className='text-3xl font-black'>سیستم پشتیبانی آموزشی خود را ملاقات کنید</h2>
+        <div className='flex flex-col md:flex-row md:justify-between w-full'>
+          <div className='flex flex-col justify-center items-center gap-4 hover:bg-white p-4'>
+            <div className='mt-4 md:mt-0'>
+              <Image src='/static/card1.webp' alt='banner' width={250} height={250} />
+            </div>
+            <h3 className='text-xl font-bold'>پشتیبانی تخصصی تحصیلی شما</h3>
+            <p className='text-center'>
+              با مشاوره ۲۴/۷، به دستاوردهای یادگیری بیشتر برسید. هر دانشجو می‌تواند سوالات خود را مطرح کند، برای
+              امتحانات آماده شود، و بهترین کارهای خود را برای هر کلاس ارائه دهد.
+            </p>
+            <Link href='/' className='text-blue-600 hover:text-blue-800'>
+              بیشتر بدانید
+            </Link>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-4 hover:bg-white p-4'>
+            <div className='mt-4 md:mt-0'>
+              <Image src='/static/card2.webp' alt='banner' width={250} height={250} />
+            </div>
+            <h3 className='text-xl font-bold'>پشتیبانی تخصصی تحصیلی شما</h3>
+            <p className='text-center'>
+              با مشاوره ۲۴/۷، به دستاوردهای یادگیری بیشتر برسید. هر دانشجو می‌تواند سوالات خود را مطرح کند، برای
+              امتحانات آماده شود، و بهترین کارهای خود را برای هر کلاس ارائه دهد.
+            </p>
+            <Link href='/' className='text-blue-600 hover:text-blue-800'>
+              بیشتر بدانید
+            </Link>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-4 hover:bg-white p-4'>
+            <div className='mt-4 md:mt-0'>
+              <Image src='/static/card3.webp' alt='banner' width={250} height={250} />
+            </div>
+            <h3 className='text-xl font-bold'>پشتیبانی تخصصی تحصیلی شما</h3>
+            <p className='text-center'>
+              با مشاوره ۲۴/۷، به دستاوردهای یادگیری بیشتر برسید. هر دانشجو می‌تواند سوالات خود را مطرح کند، برای
+              امتحانات آماده شود، و بهترین کارهای خود را برای هر کلاس ارائه دهد.
+            </p>
+            <Link href='/' className='text-blue-600 hover:text-blue-800'>
+              بیشتر بدانید
+            </Link>
+          </div>
         </div>
       </div>
     </Layout>
